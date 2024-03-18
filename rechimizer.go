6ab02ef421539efac880b437df664e8d95000819
Vladimir Mikryukov <vmikryukov@gmail.com>
2024-03-18 18:44:38 +0200

    add `hashtype` argument

--- rechimizer.go
+++ rechimizer.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
 	"crypto/sha1"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
@@ -12,17 +14,25 @@ import (
 
 	"github.com/shenwei356/bio/seq"
 	"github.com/shenwei356/bio/seqio/fastx"
+
+	"github.com/cespare/xxhash/v2"
+	"github.com/go-faster/city"
+	"github.com/spaolacci/murmur3"
 )
 
-// Define a variable for the `--headersonly` flag
+// Define variables
 var headersOnly bool
+var hashType string
 
 func main() {
 	nameFlag := flag.String("name", "", "Optional text to replace input file name in the header")
 	headersOnlyFlag := flag.Bool("headersonly", false, "If enabled, output only sequence headers")
+	hashTypeFlag := flag.String("hashtype", "sha1", "Defines the hash type: sha1 (default), md5, xxhash, cityhash, murmur3")
 	flag.Parse()
-	headersOnly = *headersOnlyFlag // Set the package-level variable based on the flag's value
-	nonFlagArgs := flag.Args()     // Get non-flag arguments
+
+	headersOnly = *headersOnlyFlag // Set variables based on the flag values
+	hashType = *hashTypeFlag
+	nonFlagArgs := flag.Args() // Get non-flag arguments
 
 	if len(nonFlagArgs) < 1 || len(nonFlagArgs) > 2 || (*nameFlag == "" && len(nonFlagArgs) == 1) {
 		fmt.Println("Usage: rechimizer [--name user_text] input_file [output_file]")
@@ -30,10 +40,11 @@ func main() {
 		fmt.Println("  Processes DNA sequences from a FASTA file, calculates a SHA1 hash for each,")
 		fmt.Println("  and outputs the modified sequences. To use standard input or output, specify '-' for the file name.")
 		fmt.Println("\nParameters:")
-		fmt.Println("  input_file   - Path to the input FASTA file or '-' for stdin.")
-		fmt.Println("  output_file  - Path to the output file or '-' for stdout. Optional; defaults to stdout if not provided.")
-		fmt.Println("  --name       - Optional. Replaces the input file name in the header with the specified text.")
+		fmt.Println("  input_file    - Path to the input FASTA file or '-' for stdin.")
+		fmt.Println("  output_file   - Path to the output file or '-' for stdout. Optional; defaults to stdout if not provided.")
+		fmt.Println("  --name        - Optional. Replaces the input file name in the header with the specified text.")
 		fmt.Println("  --headersonly - Optional. Outputs only sequence headers.")
+		fmt.Println("  --hashtype    - Optional. The hash type: sha1 (default), md5, xxhash, cityhash (as in VSEARCH), murmur3 (as in Sourmash).")
 		fmt.Println("\nExamples:")
 		fmt.Println("  rechimizer input.fasta output.fasta")
 		fmt.Println("  rechimizer --name 'Sample' - - < input.fasta > output.fasta")
@@ -105,8 +116,9 @@ func processSequences(input io.Reader, output io.Writer, inputFileName string) {
 		}
 
 		seq := record.Seq.Seq
-		sha1sum := sha1.Sum(seq)
-		modifiedHeader := fmt.Sprintf("%s;%x;%s", inputFileName, sha1sum, record.Name)
+		hashFunc := getHashFunc(hashType)
+		hashedSeq := hashFunc(seq)
+		modifiedHeader := fmt.Sprintf("%s;%s;%s", inputFileName, hashedSeq, record.Name)
 
 		if headersOnly {
 			// Output only the header, without the '>' sign, if `--headersonly` is enabled
@@ -125,5 +137,37 @@ func processSequences(input io.Reader, output io.Writer, inputFileName string) {
 
 	if err := writer.Flush(); err != nil {
 		log.Fatalf("Error flushing output: %v", err)
+	}
+}
+
+// getHashFunc returns a function that takes a byte slice and returns a hex string
+// of the hash based on the specified hash type.
+func getHashFunc(hashType string) func([]byte) string {
+	switch hashType {
+	case "md5":
+		return func(data []byte) string {
+			hash := md5.Sum(data)
+			return hex.EncodeToString(hash[:])
+		}
+	case "xxhash":
+		return func(data []byte) string {
+			hash := xxhash.Sum64(data)
+			return fmt.Sprintf("%x", hash)
+		}
+	case "cityhash":
+		return func(data []byte) string {
+			hash := city.Hash128(data)
+			return fmt.Sprintf("%x%x", hash.High, hash.Low)
+		}
+	case "murmur3":
+		return func(data []byte) string {
+			h1, h2 := murmur3.Sum128(data)
+			return fmt.Sprintf("%x%x", h1, h2)
+		}
+	default: // Default to SHA1
+		return func(data []byte) string {
+			hash := sha1.Sum(data)
+			return hex.EncodeToString(hash[:])
+		}
 	}
 }
