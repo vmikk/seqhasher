f57ab72dd04d523b3f5fd8776cc204b9dce80a9d
Vladimir Mikryukov <vmikryukov@gmail.com>
2024-09-01 11:08:20 +0300

    refactor, colorize help message

--- seqhasher.go
+++ seqhasher.go
@@ -17,6 +17,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/shenwei356/bio/seq"
 	"github.com/shenwei356/bio/seqio/fastx"
@@ -24,144 +25,159 @@ import (
 	"github.com/cespare/xxhash/v2"
 	"github.com/go-faster/city"
 	"github.com/spaolacci/murmur3"
+
+	"github.com/fatih/color"
 )
 
-// Application version
-const version = "1.0.0"
+const (
+	version         = "1.0.1" // Version of the program
+	defaultHashType = "sha1"  // Default hash type
+)
 
-// Define variables
-var headersOnly bool
-var hashType string
-var noFileName bool
-var caseSensitive bool
+var supportedHashTypes = []string{"sha1", "md5", "xxhash", "cityhash", "murmur3"}
 
-func main() {
-
-	// Check for `-v` or `--version`
-	for _, arg := range os.Args[1:] {
-		if arg == "-v" || arg == "--version" {
-			fmt.Println("seqhasher", version)
-			return
-		}
-	}
-	versionFlag := flag.Bool("version", false, "Prints the version of the program and exits")
-
-	nameFlag := flag.String("name", "", "Optional text to replace input file name in the header")
-	headersOnlyFlag := flag.Bool("headersonly", false, "If enabled, output only sequence headers")
-	hashTypeFlag := flag.String("hashtype", "sha1", "Defines the hash type: sha1, md5, xxhash, cityhash, murmur3")
-	noFileNameFlag := flag.Bool("nofilename", false, "If enabled, disables adding a file name to the sequence header")
-	caseSensitiveFlag := flag.Bool("casesensitive", false, "If enabled, keeps sequences as is without converting to uppercase")
-	flag.Parse()
-
-	// Check version
-	if *versionFlag {
-		fmt.Println("seqhasher", version)
-		return
-	}
-
-	// Set variables based on the flag values
-	headersOnly = *headersOnlyFlag
-	hashType = *hashTypeFlag
-	noFileName = *noFileNameFlag
-	caseSensitive = *caseSensitiveFlag
-
-	// Get non-flag arguments
-	nonFlagArgs := flag.Args()
-
-	// Validate hashType
-	validHashTypes := map[string]bool{
-		"sha1":     true,
-		"md5":      true,
-		"xxhash":   true,
-		"cityhash": true,
-		"murmur3":  true,
-	}
-
-	if _, valid := validHashTypes[hashType]; !valid {
-		fmt.Printf("Warning: Unsupported hash type '%s'. Using 'sha1' as default.\n", hashType)
-		hashType = "sha1" // Set to default
-	}
-
-	if len(nonFlagArgs) < 1 || len(nonFlagArgs) > 2 || (*nameFlag == "" && len(nonFlagArgs) == 1) {
-		fmt.Println("SeqHasher: DNA Sequence Hashing Tool")
-		fmt.Println("version:  ", version)
-		fmt.Println("=====================================")
-		fmt.Println("Usage:")
-		fmt.Println("  seqhasher [options] <input_file> [output_file]")
-		fmt.Println("\nOverview:")
-		fmt.Println("  SeqHasher takes DNA sequences from a FASTA file, computes a hash digest for each sequence,")
-		fmt.Println("  and generates an output file with modified sequences.")
-		fmt.Println("  For input/output via stdin/stdout, use '-' instead of the file name.")
-		fmt.Println("\nOptions:")
-		fmt.Println("  --name <text>         Replace the input file's name in the header with <text>.")
-		fmt.Println("  --nofilename          Omit the file name from the sequence header.")
-		fmt.Println("  --headersonly         Only output sequence headers, excluding the sequences themselves.")
-		fmt.Println("  --hashtype <type>     Specify the hash algorithm: sha1 (default), md5, xxhash, cityhash, or murmur3.")
-		fmt.Println("  --casesensitive       Take into account sequence case. By default, sequences are converted to uppercase.")
-		fmt.Println("  --version             Prints the version of the program and exits.")
-		fmt.Println("\nArguments:")
-		fmt.Println("  <input_file>          The path to the input FASTA file or '-' for standard input (stdin).")
-		fmt.Println("  [output_file]         The path to the output file or '-' for standard output (stdout).")
-		fmt.Println("                        If omitted, defaults to stdout.")
-		fmt.Println("\nExamples:")
-		fmt.Println("  seqhasher input.fasta output.fasta")
-		fmt.Println("  cat input.fasta | seqhasher --name 'Sample' - - > output.fasta")
-		fmt.Println("\nFor additional information, use the -h flag.")
-		return
-	}
-
-	var input io.Reader
-	var output io.Writer
-	var inputFileName string
-
-	inputFileName = nonFlagArgs[0]
-	if inputFileName == "-" {
-		input = os.Stdin
-		if *nameFlag == "" {
-			inputFileName = "stdin"
-		} else {
-			inputFileName = *nameFlag
-		}
-	} else {
-		inputFile, err := os.Open(inputFileName)
-		if err != nil {
-			log.Fatalf("Error opening input file %s: %v", inputFileName, err)
-		}
-		defer inputFile.Close()
-		input = inputFile
-		if *nameFlag != "" {
-			inputFileName = *nameFlag
-		} else {
-			inputFileName = filepath.Base(inputFileName)
-		}
-	}
-
-	if len(nonFlagArgs) == 2 {
-		if nonFlagArgs[1] == "-" {
-			output = os.Stdout
-		} else {
-			outputFile, err := os.Create(nonFlagArgs[1])
-			if err != nil {
-				log.Fatalf("Error creating output file %s: %v", nonFlagArgs[1], err)
-			}
-			defer outputFile.Close()
-			output = outputFile
-		}
-	} else {
-		output = os.Stdout
-	}
-
-	processSequences(input, output, inputFileName, caseSensitive)
+// Configuration structure (flags)
+type config struct {
+	headersOnly    bool
+	hashType       string
+	noFileName     bool
+	caseSensitive  bool
+	inputFileName  string
+	outputFileName string
+	nameOverride   string
 }
 
-func processSequences(input io.Reader, output io.Writer, inputFileName string, caseSensitive bool) {
+func main() {
+	cfg := parseFlags()
+
+	if cfg.inputFileName == "" {
+		printUsage()
+		return
+	}
+
+	input, err := getInput(cfg.inputFileName)
+	if err != nil {
+		log.Fatalf("Error opening input: %v", err)
+	}
+	defer input.Close()
+
+	output, err := getOutput(cfg.outputFileName)
+	if err != nil {
+		log.Fatalf("Error opening output: %v", err)
+	}
+	defer output.Close()
+
+	processSequences(input, output, cfg)
+}
+
+func parseFlags() config {
+	cfg := config{}
+
+	flag.BoolVar(&cfg.headersOnly, "headersonly", false, "Output only headers")
+	flag.StringVar(&cfg.hashType, "hash", defaultHashType, "Hash type (sha1, md5, xxhash, cityhash, murmur3)")
+	flag.BoolVar(&cfg.noFileName, "nofilename", false, "Do not include file name in output")
+	flag.BoolVar(&cfg.caseSensitive, "casesensitive", false, "Case-sensitive hashing")
+	flag.StringVar(&cfg.nameOverride, "name", "", "Override input file name in output")
+
+	flag.Usage = printUsage
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) > 0 {
+		cfg.inputFileName = args[0]
+	}
+	if len(args) > 1 {
+		cfg.outputFileName = args[1]
+	}
+
+	// Validate hash type
+	if !isValidHashType(cfg.hashType) {
+		log.Fatalf("Invalid hash type: %s. Supported types are: %s", cfg.hashType, strings.Join(supportedHashTypes, ", "))
+	}
+
+	return cfg
+}
+
+func isValidHashType(hashType string) bool {
+	for _, supported := range supportedHashTypes {
+		if hashType == supported {
+			return true
+		}
+	}
+	return false
+}
+
+func getInput(fileName string) (io.ReadCloser, error) {
+	if fileName == "" || fileName == "-" {
+		return os.Stdin, nil
+	}
+	return os.Open(fileName)
+}
+
+func getOutput(fileName string) (io.WriteCloser, error) {
+	if fileName == "" || fileName == "-" {
+		return os.Stdout, nil
+	}
+	return os.Create(fileName)
+}
+
+func printUsage() {
+	if len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help") {
+		fmt.Printf("\n%s%s%s\n",
+			color.HiGreenString("SeqHasher"),
+			color.WhiteString(" : "),
+			color.HiMagentaString("DNA Sequence Hashing Tool"))
+		fmt.Printf("%s  %s\n", color.HiCyanString("version:"), color.WhiteString(version))
+		fmt.Println(color.WhiteString("====================================="))
+		fmt.Println(color.HiCyanString("Usage:"))
+		fmt.Printf("  %s\n", color.WhiteString("seqhasher [options] <input_file> [output_file]"))
+		fmt.Println(color.HiCyanString("\nOverview:"))
+		fmt.Println(color.WhiteString("  SeqHasher takes DNA sequences from a FASTA file, computes a hash digest for each sequence,"))
+		fmt.Println(color.WhiteString("  and generates an output file with modified sequences."))
+		fmt.Println(color.WhiteString("  For input/output via stdin/stdout, use '-' instead of the file name."))
+		fmt.Println(color.HiCyanString("\nOptions:"))
+		fmt.Printf("  %s %s\n", color.HiMagentaString("--nofilename"), color.WhiteString("    Omit the file name from the sequence header"))
+		fmt.Printf("  %s %s\n", color.HiMagentaString("--name <text>"), color.WhiteString("   Replace the input file's name in the header with <text>"))
+		fmt.Printf("  %s %s\n", color.HiMagentaString("--headersonly"), color.WhiteString("   Only output sequence headers, excluding the sequences themselves"))
+		fmt.Printf("  %s %s\n", color.HiMagentaString("--hash <type>"), color.WhiteString("   Hash algorithm: sha1 (default), md5, xxhash, cityhash, or murmur3"))
+		fmt.Printf("  %s %s\n", color.HiMagentaString("--casesensitive"), color.WhiteString(" Take into account sequence case. By default, sequences are converted to uppercase."))
+		fmt.Printf("  %s %s\n", color.HiMagentaString("--version"), color.WhiteString("       Prints the version of the program and exits"))
+		fmt.Println(color.HiCyanString("\nArguments:"))
+		fmt.Printf("  %s %s\n", color.HiMagentaString("<input_file>"), color.WhiteString("    The path to the input FASTA file or '-' for standard input (stdin)"))
+		fmt.Printf("  %s %s\n", color.HiMagentaString("[output_file]"), color.WhiteString("   The path to the output file or '-' for standard output (stdout)"))
+		fmt.Println(color.WhiteString("                   If omitted, defaults to stdout."))
+		fmt.Println(color.HiCyanString("\nExamples:"))
+		fmt.Println(color.WhiteString("  seqhasher input.fasta output.fasta"))
+		fmt.Println(color.WhiteString("  cat input.fasta | seqhasher --name 'Sample' - - > output.fasta"))
+	} else {
+		fmt.Fprintf(os.Stderr, "SeqHasher v%s\n", version)
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] <input_file> [output_file]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\nSupported hash types: %s\n", strings.Join(supportedHashTypes, ", "))
+		fmt.Fprintf(os.Stderr, "If input_file is '-' or omitted, reads from stdin.\n")
+		fmt.Fprintf(os.Stderr, "If output_file is '-' or omitted, writes to stdout.\n")
+		fmt.Fprintf(os.Stderr, "\nFor more detailed help, use -h or --help.\n")
+	}
+}
+
+func processSequences(input io.Reader, output io.Writer, cfg config) {
 	writer := bufio.NewWriter(output)
 
 	reader, err := fastx.NewReaderFromIO(seq.DNA, bufio.NewReader(input), fastx.DefaultIDRegexp)
 	if err != nil {
 		log.Fatalf("Failed to create reader: %v", err)
 	}
-	defer reader.Close() // Ensure to close the reader when done
+	defer reader.Close() // Close the reader after processing
+
+	inputFileName := cfg.inputFileName
+	if cfg.nameOverride != "" {
+		inputFileName = cfg.nameOverride
+	} else if inputFileName != "-" {
+		inputFileName = filepath.Base(inputFileName)
+	}
+
+	hashFunc := getHashFunc(cfg.hashType)
 
 	for {
 		record, err := reader.Read()
@@ -174,23 +190,22 @@ func processSequences(input io.Reader, output io.Writer, inputFileName string, c
 		}
 
 		seq := record.Seq.Seq
-		if !caseSensitive {
-			// Convert to uppercase if caseSensitive is false
+		if !cfg.caseSensitive {
+			// Convert sequence to uppercase if case-insensitive hashing is enabled
 			seq = bytes.ToUpper(seq)
 		}
 
-		hashFunc := getHashFunc(hashType)
 		hashedSeq := hashFunc(seq)
 
 		// Prepare the new sequence header
 		var modifiedHeader string
-		if noFileName {
+		if cfg.noFileName {
 			modifiedHeader = fmt.Sprintf("%s;%s", hashedSeq, record.Name)
 		} else {
 			modifiedHeader = fmt.Sprintf("%s;%s;%s", inputFileName, hashedSeq, record.Name)
 		}
 
-		if headersOnly {
+		if cfg.headersOnly {
 			// Output only the header, without the '>' sign, if `--headersonly` is enabled
 			if _, err := fmt.Fprintf(writer, "%s\n", modifiedHeader); err != nil {
 				log.Printf("Error writing header: %v", err)
